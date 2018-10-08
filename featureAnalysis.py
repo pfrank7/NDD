204c057588d5edc07d8b2d97804aeed14f8b0c2f
Paige Frank <paigefrank@Paiges-MacBook-Pro.local>
2018-10-08 19:53:54 -0400

    selects features with highest variance

--- featureAnalysis.py
+++ featureAnalysis.py
@@ -11,15 +11,29 @@ class featureSelection():
     def eigenvectorCentrality(self, numFeatures = 15):
         #computes eigen vector centrality for the graph and returns a np matrix containing
         #eigenvector centrality values for each graph
-        #numFeatures selects the number of values to return for each graph, returns values for the
-        #first numFeatures nodes
-        #default numFeatures is 15
+        #numFeatures selects the number of values to return for each graph, returns values for the features with highest variance
         featureMatrix = []
         for graph in self.graph_list:
             centrality = nx.eigenvector_centrality(graph)
             eigenVectorDict = dict(sorted((v, '{:0.2f}'.format(c)) for v, c in centrality.items()))
-            list_partial = list(eigenVectorDict.values())[0:numFeatures]
-            if len(list_partial) == numFeatures:
-                featureMatrix.append(list_partial)
-        return np.stack(featureMatrix)
+            list_ = list(eigenVectorDict.values())
+            if len(list_) == self.numNodes:
+                featureMatrix.append(list_)
+        featureMatrix = np.stack(featureMatrix).astype("float")
+        varValues = self.getVariance(featureMatrix)
+        featInds = np.argsort(varValues)[-numFeatures:]
+        return featureMatrix[:,featInds]
+    
+    def getVariance(self, featureMatrix):
+        varValues = []
+        for i in range(0, featureMatrix.shape[1]):
+            values = featureMatrix[:,i]
+            meanSample = np.mean(values)
+            sumSample = 0
+            for j in range(0, featureMatrix.shape[0]):
+                sumSample += (featureMatrix[j,i] - meanSample)**2
+            varValues.append(sumSample/(featureMatrix.shape[0] - 1))
+        return varValues
+            
+        
 
