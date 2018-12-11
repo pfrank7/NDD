b243483a28de7e62c4c4af8a0b199874050e6b8d
Paige Frank <paigefrank@Paiges-MacBook-Pro.local>
2018-12-11 11:00:46 -0500

    updated R code

--- UrerfSimMat.R
+++ UrerfSimMat.R
@@ -1,23 +1,18 @@
-
-# make sure to put the correct path to rfr_us.R
 source('~/Documents/U-Rerf/rfr_us.R')
+  
+  "number of trees for forest"
+  numtrees <- 500
+  "the 'k' of k nearest neighbors"
+  k <- 10
+  "set max depth"
+  depth <- 4
+  
+  "D = num graphs, m = iris features"
+  X<-as.matrix(read.table("~/Documents/NDD/output/outfileMat_HNU_d.txt",header=FALSE,sep=" "))
 
-# number of trees for forest
-numtrees <- 100
-# the 'k' of k nearest neighbors
-k <- 10
-# set max depth
-depth <- 4
-
-#D = num graphs, m = eigenvector centrality values
-X<-as.matrix(read.table("~/Documents/NDD/outfileMat.txt",header=FALSE,sep=" "))
-
-#X<-as.matrix(read.table("~/Documents/NDD/outTestMat.txt",header=FALSE,sep=" "))
-
-# create a urerf structure which includes the similarity matrix
-urerfStructure <- urerf(X, numtrees, k)
-SM = urerfStructure$similarityMatrix
-write.table(SM, file="~/Documents/NDD/outfileSimMat.txt", row.names=FALSE, col.names=FALSE)
-
-#write.table(SM, file="~/Documents/NDD/inTestMat.txt", row.names=FALSE, col.names=FALSE)
+  "create a urerf structure which includes the similarity matrix"
+  urerfStructure <- urerf(X, numtrees, k)
+  SM = urerfStructure$similarityMatrix
+  
+  write.table(SM, file="~/Documents/NDD/input/outfileSimMat_HNU_d.txt", row.names=FALSE, col.names=FALSE)
 
